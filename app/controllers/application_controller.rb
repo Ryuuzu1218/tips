ae7025188ee9891065e1b17c4eb710a9f6305d92
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-06-11 19:57:57 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_with_http_digest
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth,if: :production?
-
+  before_action :basic_auth, if: :production?
   protected
 
   def configure_permitted_parameters
@@ -12,9 +11,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def production?
-    Rails.env.production?
-  end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
