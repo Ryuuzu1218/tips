80fcd8070bfb32dad9e90c5b28757fecf824e9e7
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-06-14 17:59:24 +0900

    リファクタリングとREADME

--- spec/factories/memos.rb
+++ spec/factories/memos.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :memo do
     title         {"タイトル"}
     body          {"内容がここに入る"}
-    link          {"https://qiita.com/sanriyochan/items/8ef84f92f8685e1c74a0"}
+    link          {"https://qiita.com/"}
     updated_at { Faker::Time.between(from: DateTime.now - 4, to: DateTime.now) }
   end
 end
--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 describe Memo do
   describe "#create" do
-    it "タイトルがないと保存できないでござる" do
+    it "タイトルがないと保存できない" do
       memo=FactoryBot.build(:memo,title:"")
       memo.valid?
       expect(memo.errors[:title]).to include("can't be blank")
     end
-    it "内容がないと保存できないでござる" do
+    it "内容がないと保存できない" do
       memo=FactoryBot.build(:memo,body:"")
       memo.valid?
       expect(memo.errors[:body]).to include("can't be blank")
     end
-    it "内容が5文字未満だと保存できないでござる" do
+    it "内容が5文字未満だと保存できない" do
       memo=FactoryBot.build(:memo,body:"４文字")
       memo.valid?
       expect(memo.errors[:body]).to include("is too short (minimum is 5 characters)")
